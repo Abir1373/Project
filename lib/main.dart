acb5ba036eeed34dc14c3f35cc7be7b476370d77
Abir1373 <abirm3188@gmail.com>
2023-01-17 22:24:40 +0600

    modify

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,64 @@ class HomeOne extends StatelessWidget {
                     borderRadius: BorderRadius.all(Radius.circular(30))),
               ),
               Positioned(
-                top: 323,
+                top: 280,
                 left: 30,
                 child: Container(
                   height: 250,
                   width: 340,
+                  padding: EdgeInsets.all(20),
                   decoration: BoxDecoration(
-                    color: Colors.grey,
+                    color: Colors.white,
+                    border: Border(
+                      top: BorderSide(color: Colors.black),
+                      right: BorderSide(color: Colors.black),
+                      left: BorderSide(color: Colors.black),
+                      bottom: BorderSide(color: Colors.black),
+                    ),
                     borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "LOG IN",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueGrey,
+                                ),
+                              ),
+                              Container(
+                                height: 2,
+                                width: 55,
+                                color: Colors.black,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "SIGN UP",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blueGrey,
+                                ),
+                              ),
+                              Container(
+                                height: 2,
+                                width: 55,
+                                color: Colors.black,
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
               ),
